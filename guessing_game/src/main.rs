90675aca041ea6ba291747c1ae82f27a6511f179
Bogdan Arabadzhi <bogdan.today@gmail.com>
2017-05-16 12:38:57 +0300

    Add should_panic exercise

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -10,8 +10,12 @@ struct Guess {
 
 impl Guess {
     pub fn new(value: u32) -> Guess {
-        if value < 1 || value > 100 {
-            panic!("Guess value must be between 1 and 100, got {}.", value);
+        if value < 1 {
+            panic!("Guess value must be greater than or equal to 1, got {}.",
+                   value);
+        } else if value > 100 {
+            panic!("Guess value must be less than or equal to 100, got {}.",
+                   value);
         }
 
         Guess { value }
@@ -51,5 +55,16 @@ fn main() {
                 break;
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    #[should_panic(expected = "Guess value must be less than or equal to 100")]
+    fn greater_than_100() {
+        Guess::new(200);
     }
 }
