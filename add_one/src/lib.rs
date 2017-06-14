c9a4ffc3e347f93da62cf01a58a576c446281326
Bogdan Arabadzhi <bogdan.today@gmail.com>
2017-06-14 23:16:00 +0300

    Add documentation comments exercise

---
+++ add_one/src/lib.rs
@@ -0,0 +1,12 @@
+/// Adds one to the number given.
+///
+/// # Examples
+///
+/// ```
+/// let five = 5;
+///
+/// assert_eq!(6, add_one(five));
+/// ```
+pub fn add_one(x: i32) -> i32 {
+    x + 1
+}
